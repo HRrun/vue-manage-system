1402211afcf4a0c9098710907f430cda39cf0aa9
HRrun <hr_run@163.com>
2021-05-13 16:46:24 +0800

    cmmmit

--- vue.config.js
+++ vue.config.js
@@ -1,16 +1,17 @@
 module.exports = {
-    baseUrl: './',
+    publicPath: './',
     assetsDir: 'static',
     productionSourceMap: false,
-    // devServer: {
-    //     proxy: {
-    //         '/api':{
-    //             target:'http://jsonplaceholder.typicode.com',
-    //             changeOrigin:true,
-    //             pathRewrite:{
-    //                 '/api':''
-    //             }
-    //         }
-    //     }
-    // }
+    devServer: {
+        proxy: {
+            '/api':{
+                target:'http://192.168.0.5:8001',
+                //target:'http://www.kmjn.net.cn:8001',
+                changeOrigin:false,
+                pathRewrite:{
+                    '^/api':''
+                }
+            }
+        }
+    }
 }
